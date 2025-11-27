ae00bf7b5ebb2705403c65cc319c7b5bfa215ce8
faizol <faizol@mdd.co.id>
2025-11-27 23:42:26 +0700

    add ornaments

--- src/components/BottomOrnaments.jsx
+++ src/components/BottomOrnaments.jsx
@@ -1,9 +1,20 @@
 import BgLeftBottom from '@/images/bg-left-bottom.png';
 import BgRightBottom from '@/images/bg-right-bottom.png';
 import BgBottom from '@/images/bg-bottom.png';
+import BgClown from '@/images/bg-clown.png';
 
 const BottomOrnaments = () => (
   <>
+    {/* Center overlay above left/right */}
+    <div className="pointer-events-none absolute inset-x-0 bottom-0 flex justify-center">
+      <img
+        src={BgClown}
+        alt=""
+        className="w-full max-w-[560px] sm:max-w-[700px] object-contain"
+        loading="lazy"
+      />
+    </div>
+
     <div className="pointer-events-none absolute bottom-0 left-0">
       <img
         src={BgLeftBottom}
