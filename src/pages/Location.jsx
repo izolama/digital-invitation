ab54e211d361eee69356121a60d10b18d10a4529
faizol <faizol@mdd.co.id>
2025-12-01 11:59:33 +0700

    fix texting

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -6,7 +6,7 @@ import BottomOrnaments from '@/components/BottomOrnaments';
 
 const timeline = [
     { title: 'Registration', time: '18:00 WIB' },
-    { title: 'Opening', time: '19:00 WIB' },
+    { title: 'Opening & Greeting', time: '19:00 WIB' },
     { title: 'Dinner', time: '20:00 WIB' },
     { title: 'Awarding', time: '20:30 WIB' },
     { title: 'Closing', time: '22:00 WIB' },
